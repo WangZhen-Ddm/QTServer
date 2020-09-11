af3acd6f11f07bb6165afa4ce8e8df8d43717cca
WangZhen-Ddm <846958884@qq.com>
2020-09-11 10:47:53 +0800

    fix: //

--- src/main/java/com/qt/demo/DemoApplication.java
+++ src/main/java/com/qt/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.qt.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public HttpFirewall defaultHttpFirewall() {
+        return new DefaultHttpFirewall();
     }
 
 }
